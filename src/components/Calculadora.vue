d7b97ebca1e0fdd554884f38d1946a56e320ca25
Polyana Feitosa <45500017+PolyanaF@users.noreply.github.com>
2022-09-23 20:20:29 -0300

    Adiconando perfumaria na calculadora - Calculadora.vue.

--- src/components/Calculadora.vue
+++ src/components/Calculadora.vue
@@ -1,26 +1,26 @@
 <template>
   <div class="calculadora">
    <div class="display">0</div>
-   <div>%</div>
-   <div>%</div>
-   <div>C</div>
-   <div>÷</div>
-   <div>7</div>
-   <div>8</div>
-   <div>9</div>
-   <div>x</div>
-   <div>4</div>
-   <div>5</div>
-   <div>6</div>
-   <div>-</div>
-   <div>1</div>
-   <div>2</div>
-   <div>3</div>
-   <div>+</div>
-   <div>+/-</div>
-   <div>0</div>
-   <div>,</div>
-   <div>=</div>
+   <div class="botao">%</div>
+   <div class="botao">√</div>
+   <div class="botao">C</div>
+   <div class="botao">÷</div>
+   <div class="botaoNum">7</div>
+   <div class="botaoNum">8</div>
+   <div class="botaoNum">9</div>
+   <div class="botao">x</div>
+   <div class="botaoNum">4</div>
+   <div class="botaoNum">5</div>
+   <div class="botaoNum">6</div>
+   <div class="botao">-</div>
+   <div class="botaoNum">1</div>
+   <div class="botaoNum">2</div>
+   <div class="botaoNum">3</div>
+   <div class="botao">+</div>
+   <div class="botao">+/-</div>
+   <div class="botaoNum">0</div>
+   <div class="botao">,</div>
+   <div class="botaoResul">=</div>
   </div>
 </template>
 
@@ -33,7 +33,7 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 .calculadora{
-  font-size: 40px;
+  font-size: 15px;
   display: grid;
   grid-template-columns: repeat(4, 1fr);
   grid-auto-rows: minmax(50px, auto);
@@ -42,6 +42,21 @@ export default {
 .display{
   grid-column: 1 / 5;
   background-color: #333;
+  color: white;
+}
+
+.botao{
+  background-color: #afafaf;
+  border: 1px solid rgb(119, 119, 119);
+}
+
+.botaoNum {
+  border: 1px solid rgb(119, 119, 119);
+}
+
+.botaoResul{
+  background-color: orangered;
+  border: 1px solid rgb(119, 119, 119);
   color: white;
 }
 </style>
