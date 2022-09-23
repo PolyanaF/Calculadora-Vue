df6b9ba00cab4c94274372b0f653117d2a599b3c
Polyana Feitosa <45500017+PolyanaF@users.noreply.github.com>
2022-09-23 19:53:00 -0300

    Criando botões da calculadora - Calculadora.vue.

--- src/components/Calculadora.vue
+++ src/components/Calculadora.vue
@@ -1,17 +1,26 @@
 <template>
   <div class="calculadora">
-   <div>teste</div>
-   <div>teste</div>
-   <div>teste</div>
-   <div>teste</div>
-   <div>teste</div>
-   <div>teste</div>
-   <div>teste</div>
-   <div>teste</div>
-   <div>teste</div>
-   <div>teste</div>
-   <div>teste</div>
-   <div>teste</div>
+   <div class="display">0</div>
+   <div>%</div>
+   <div>%</div>
+   <div>C</div>
+   <div>÷</div>
+   <div>7</div>
+   <div>8</div>
+   <div>9</div>
+   <div>x</div>
+   <div>4</div>
+   <div>5</div>
+   <div>6</div>
+   <div>-</div>
+   <div>1</div>
+   <div>2</div>
+   <div>3</div>
+   <div>+</div>
+   <div>+/-</div>
+   <div>0</div>
+   <div>,</div>
+   <div>=</div>
   </div>
 </template>
 
@@ -23,11 +32,16 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
 .calculadora{
-
+  font-size: 40px;
   display: grid;
   grid-template-columns: repeat(4, 1fr);
   grid-auto-rows: minmax(50px, auto);
+}
+
+.display{
+  grid-column: 1 / 5;
+  background-color: #333;
+  color: white;
 }
 </style>
